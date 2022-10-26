a842b101b4a14a3899258578c4d4e1c34ce3c104
dabhade904 <dabhade904@gmail.com>
2022-10-26 10:53:59 +0530

    Added : routing functionality to navigate between the different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 const routes: Routes = [
   {path:'registration',component:RegisterComponent},
   { path:'login',component:LoginComponent},
   {path:'forgetpassword',component:ForgetPasswordComponent},
-  {path:'resetpassword',component:RegisterComponent}
+  {path:'resetpassword',component:ResetPasswordComponent}
 ];
 
 @NgModule({
