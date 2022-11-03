f24cacd80b764614ac050336b335be0aa3eba4b0
dabhade904 <dabhade904@gmail.com>
2022-11-03 23:13:51 +0530

    Added : activated routing functionality to unarchive and restore

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NoteService } from 'src/app/services/noteService/note.service';
-
+import { ActivatedRoute } from '@angular/router';
+import { TrashComponent } from '../trash/trash.component';
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
@@ -8,14 +9,22 @@ import { NoteService } from 'src/app/services/noteService/note.service';
 })
 export class IconsComponent implements OnInit {
   @Input() noteCard: any;
-  constructor(private note: NoteService) { }
+  isArchive: boolean = false;
+  isTrash: boolean = false;
+  constructor(private note: NoteService,private activatedroute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let Component = this.activatedroute.snapshot.component;
+    if (Component == TrashComponent) {
+      this.isTrash = true;
+    }
+    if (Component == TrashComponent) {
+      this.isArchive = true;
+    }
   }
-  onArchiev() {
+  onClickArchive() {
     let data = {
       noteID: [this.noteCard.noteID]
-
     }
     console.log(data);
     this.note.archiveNote(data).subscribe((Response: any) => {
@@ -32,4 +41,22 @@ export class IconsComponent implements OnInit {
       console.log(Response);
     })
   }
+  onUnArchievenote() {
+    let reqdata = {
+      noteID: [this.noteCard.noteID],
+    }
+    this.note.archiveNote(reqdata).subscribe((response: any) => {
+      console.log(response);
+    })
+  }
+  onRestore() {
+    let reqdata = {
+      noteID: [this.noteCard.noteID],
+      isTrash: false,
+    }
+    this.note.trashNote(reqdata).subscribe((response: any) => {
+      console.log(response);
+    })
+  }
 }
+
